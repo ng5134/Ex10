20ce4b125371c416274fd06b7861e80a5a8268d4
ng5134 <ng5134@bs.amalnet.k12.il>
2025-03-31 13:39:59 +0300

    Java

--- app/src/main/java/com/example/ex10/MainActivity.java
+++ app/src/main/java/com/example/ex10/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.ex10;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +17,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    ToggleButton tb;
+    Switch switch1;
+     LinearLayout m;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tb = (ToggleButton) findViewById(R.id.tb);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        m = (LinearLayout) findViewById(R.id.m);
+    }
+
+    public void click1(View view)
+    {
+        if (bt.isChecked() && !(switch1.isChecked()) )
+        {
+            m.setBackgroundColor(Color.RED);
+        }
+        else if (bt.isChecked() && (switch1.isChecked()) )
+        {
+            m.setBackgroundColor(Color.BLUE);
+        }
+        else if (!(bt.isChecked()) && !(switch1.isChecked()) )
+        {
+            m.setBackgroundColor(Color.BLACK);
+        }
+        else
+        {
+            m.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
